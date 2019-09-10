02dff9ed3ad26a216e016794d1b384da3747a5da
Ishtiaqasif <ishtiaqasif03@gmail.com>
2019-09-10 19:45:53 +0600

    Switching fixed for new and old user(Initial Commit)

---
+++ app/src/main/java/com/app/ecommerce/activities/ActivitySplash.java
@@ -0,0 +1,116 @@
+package com.app.ecommerce.activities;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.content.Context;
+
+import com.app.ecommerce.Config;
+import com.app.ecommerce.R;
+import com.app.ecommerce.ui.login.LoginActivity;
+import com.app.ecommerce.utilities.SharedPref;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    Boolean isCancelled = false;
+    private ProgressBar progressBar;
+    long id = 0;
+    String url = "";
+
+    final String ageY = "Young";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Context context =  getApplicationContext();
+        final SharedPref sharedPref = new SharedPref(context);
+
+
+        if (Config.ENABLE_RTL_MODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+            }
+        }
+
+
+
+        progressBar = findViewById(R.id.progressBar);
+
+        if (getIntent().hasExtra("nid")) {
+            id = getIntent().getLongExtra("nid", 0);
+            url = getIntent().getStringExtra("external_link");
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                progressBar.setVisibility(View.GONE);
+
+                if (!isCancelled) {
+                    if (id == 0) {
+                        if (url.equals("") || url.equals("no_url")) {
+
+                            boolean diff = sharedPref.getYourAge().equals(ageY);
+
+                            if(diff){
+                                Intent x = new Intent(ActivitySplash.this, LoginActivity.class);
+                                startActivity(x);
+                                finish();
+                                }
+                            else if(!diff){
+
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+
+                        } else {
+
+                            Intent a = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(a);
+
+                            Intent b = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                            startActivity(b);
+
+                            finish();
+                        }
+                    } else if (id == 1010101010) {
+
+                        Intent a = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(a);
+
+                        Intent b = new Intent(getApplicationContext(), ActivityHistory.class);
+                        startActivity(b);
+
+                        finish();
+
+                    } else {
+
+                        Intent a = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(a);
+
+                        Intent b = new Intent(getApplicationContext(), ActivityNotificationDetail.class);
+                        b.putExtra("product_id", id);
+                        startActivity(b);
+
+                        finish();
+
+                    }
+                }
+            }
+        }, Config.SPLASH_TIME);
+
+    }
+
+}
